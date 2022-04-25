0bf751cc2522be9ab8b23fd7fbff853d0783f20f
Igor Matuszewski <xanewok@gmail.com>
2022-04-25 18:46:46 +0200

    Use dummy data

--- app/src/components/MarketItem/index.tsx
+++ app/src/components/MarketItem/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Img, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Img, Text } from '@chakra-ui/react'
 
 export const MarketItem = (props: {
   img: any
@@ -9,8 +9,61 @@ export const MarketItem = (props: {
   allocatedSpots: any
 }) => {
   return (
-    <Flex h="300pxw" w="500px" p="5" box-sizing="border-box" bg="black">
-      <Img src={props.img} height="200" width="120" />
+    <Flex
+      h="300pxw"
+      w={['325px', '500px']}
+      p={5}
+      bg="#16112d"
+      boxShadow="0 -3px 0 0 #231b4d,0 3px 0 0 #231b4d,-3px 0 0 0 #231b4d,3px 0 0 0 #231b4d,0 0 0 3px #0a0414,0 -6px 0 0 #0a0414,0 6px 0 0 #0a0414,-6px 0 0 0 #0a0414,6px 0 0 0 #0a0414"
+    >
+      <Flex direction="row">
+        <Box w="165px" h="165px" style={{ aspectRatio: '1/1' }} mr={4}>
+          <Img
+            // maxH="100%"
+            // maxW="100%"
+            // objectFit="scale-down"
+            boxShadow="0 -3px 0 0 #3e2e6c,0 3px 0 0 #3e2e6c,-3px 0 0 0 #3e2e6c,3px 0 0 0 #3e2e6c"
+            src="/moonbirds.png"
+          />
+        </Box>
+      </Flex>
+      <Flex direction="column">
+        <Heading textColor="white" lineHeight="24px" mb={3}>
+          Moonbirds
+        </Heading>
+        <Text textColor="#AE9ED1" fontWeight="500">
+          A collection of 10,000 utility-enabled PFPs that feature a richly
+          diverse and unique pool of rarity-powered traits. What's more, each
+          Moonbird unlocks private club membership and additional benefits the
+          longer you hold them.
+        </Text>
+        <Flex direction="row">
+          <Text as="span" textColor="#AE9ED1" fontWeight="500" mr={3}>
+            7/30
+          </Text>
+          <Flex>
+            <Text
+              as="span"
+              textColor="#AE9ED1"
+              fontWeight="500"
+              pb={1}
+              mr={1.5}
+            >
+              500
+            </Text>
+            <Img
+              w="26px"
+              h="26px"
+              style={{ imageRendering: 'pixelated' }}
+              src="/cfti.png"
+            ></Img>
+          </Flex>
+        </Flex>
+        <Button mt="2" w="max(50%, 100px)" fontSize="xl">
+          Redeem
+        </Button>
+      </Flex>
+
       <Box>
         <Text fontSize="3xl" color="white">
           {props.name}
@@ -18,17 +71,6 @@ export const MarketItem = (props: {
         <Text fontSize="xl" color="white">
           {props.content}
         </Text>
-
-        <Flex gap="5">
-          <Text fontSize="xl" color="white">
-            {props.allocatedSpots}/{props.spots}
-          </Text>
-          <Text fontSize="xl" color="white">
-            {props.price} $CFTI
-          </Text>
-        </Flex>
-
-        <Button>Participate!</Button>
       </Box>
     </Flex>
   )
--- app/src/components/Marketplace/index.tsx
+++ app/src/components/Marketplace/index.tsx
@@ -6,17 +6,26 @@ import {
   Container,
   Box,
   Text,
+  Grid,
 } from '@chakra-ui/react'
 import { useEthers, useGasPrice } from '@usedapp/core'
+import { useState } from 'react'
 import { InfoShield } from '../InfoShield'
+import { MarketItem } from '../MarketItem'
 import Status from '../Status'
 
-const TabItem: React.FC<{ value: string; active?: boolean }> = (props) => (
+const TabItem: React.FC<{
+  value: string
+  active?: boolean
+  onClick: () => void
+}> = (props) => (
   <Box
     px={2}
     h="40px"
     color="#5e578a"
     cursor="pointer"
+    userSelect="none"
+    onClick={props.onClick}
     background={props.active ? '#2b2254' : '#16112d'}
     boxShadow={
       props.active
@@ -40,6 +49,10 @@ export const Marketplace: React.FC = () => {
 
   const { account } = useEthers()
 
+  const [activeTab, setActiveTab] = useState<
+    'raffles' | 'whitelists' | 'rewards'
+  >('raffles')
+
   return (
     <Box position="relative" h="100%" w="100%" overflow="auto">
       <Flex position="absolute" top={10} right={10} zIndex={10}>
@@ -60,14 +73,38 @@ export const Marketplace: React.FC = () => {
         <Heading textColor="white" lineHeight="36px" mb={0}>
           Market
         </Heading>
-        <Heading fontWeight="normal" textColor="#5E578A" lineHeight={3} mb={7} fontSize="2xl">
+        <Heading
+          fontWeight="normal"
+          textColor="#5E578A"
+          lineHeight={3}
+          mb={7}
+          fontSize="2xl"
+        >
           Spend your hard-earned CFTI on prizes!
         </Heading>
         <Flex gap={4} mb={10}>
-          <TabItem value="Raffles" />
-          <TabItem value="Whitelists" active />
-          <TabItem value="Rewards" />
+          <TabItem
+            value="Raffles"
+            active={activeTab === 'raffles'}
+            onClick={() => setActiveTab('raffles')}
+          />
+          <TabItem
+            value="Whitelists"
+            active={activeTab === 'whitelists'}
+            onClick={() => setActiveTab('whitelists')}
+          />
+          <TabItem
+            value="Rewards"
+            active={activeTab === 'rewards'}
+            onClick={() => setActiveTab('rewards')}
+          />
         </Flex>
+        <Grid
+          gap={10}
+          gridTemplateColumns="repeat(auto-fill,minmax(500px,1fr))"
+        >
+          {activeTab === 'raffles' ? [0, 1, 2].map(MarketItem) : []}
+        </Grid>
       </Box>
     </Box>
   )
